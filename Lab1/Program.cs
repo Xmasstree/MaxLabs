01fb3c6c622bdcebd268d05883289ecc8a49b811
Ilya Smolnikov <ilya.sortym@gmail.com>
2024-05-09 15:26:01 +0500

    init

---
+++ Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            if (a.Length % 2 == 0)
+            {
+                string b = a.Substring(0, a.Length / 2);
+                char[] bc = b.ToCharArray();
+                Array.Reverse(bc);
+                a = a.Substring(a.Length / 2);
+                char[] ac = a.ToCharArray();
+                Array.Reverse(ac);
+                Console.WriteLine(String.Concat<char>(bc) + String.Concat<char>(ac));
+                Console.ReadLine();
+            }
+            else
+            {
+                char[] b = a.ToCharArray();
+                Array.Reverse(b);
+                Console.WriteLine(String.Concat<char>(b) + a);
+                Console.ReadLine();
+            }
+        }
+    }
+}
